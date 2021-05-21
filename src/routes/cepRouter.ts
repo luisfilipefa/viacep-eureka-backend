daa6effe11eddde1cd617c5f539da53df86ec5a1
Luis Filipe F. Almeida <luisfilipe.faw@gmail.com>
2021-05-21 10:32:38 -0300

    Add comments to cepRouter.ts

--- src/routes/cepRouter.ts
+++ src/routes/cepRouter.ts
@@ -2,6 +2,8 @@ import { cacheMiddleware } from "../middlewares/cacheMiddleware";
 import express from "express";
 import { getCepInfo } from "../controllers/cepController";
 
+// Cria um novo router utilizado pela rota /api/cep
 export const cepRouter = express.Router();
 
+// Cria uma nova rota do método GET que recebe um cep como parâmetro
 cepRouter.get("/:cep", cacheMiddleware, getCepInfo);
