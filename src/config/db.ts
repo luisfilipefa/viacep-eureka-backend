0bcc923b1a66ebf8726e154eebdfca8c30277489
Luis Filipe F. Almeida <luisfilipe.faw@gmail.com>
2021-05-21 10:47:14 -0300

    Add comments to db.ts

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,5 +1,7 @@
 import mongoose from "mongoose";
 
+// Função recebe um parâmetro uri e o usa para se conectar ao banco utilizando o mongoose.connect,
+// e retorna a conexão
 export const connectDB = (uri: string) => {
   mongoose.connect(uri, {
     useNewUrlParser: true,
