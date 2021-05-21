8e5ac0cfe0dce66112daee69b2960f691a3e8c51
Luis Filipe F. Almeida <luisfilipe.faw@gmail.com>
2021-05-21 10:45:46 -0300

    Add comments to cepController.ts

--- src/controllers/cepController.ts
+++ src/controllers/cepController.ts
@@ -1,35 +1,38 @@
+import { CepInfo, CepInfoModel } from "../models/CepInfoModel";
 import { Request, Response } from "express";
 
-import { CepInfoModel } from "../models/CepInfoModel";
 import { api } from "../services/api";
 
+// Função que recebe um cep como parâmetro nos dados da requisição e
+// faz uma chamada ao serviço ViaCep para buscar os dados do cep desejado.
+// Caso o serviço retorne um erro (cep não encontrado), é retornado um status 400
+// com um código e uma mensagem, para ser tratado no frontend. Caso o serviço retorne
+// os dados do cep, a informação é cacheada no banco para requisições posteriores, e
+// o novo documento criado no banco é retornado
 export const getCepInfo = async (req: Request, res: Response) => {
   try {
     const { data } = await api.get(`/${req.params.cep}/json`);
 
     if (data.erro) {
-      return res.status(200).json({
+      return res.status(400).json({
         error: true,
         code: "viacep.not_found",
         message: "Resource not found in ViaCep API",
-        data,
       });
     }
 
-    const cep = {
+    const cepInfo = await CepInfoModel.create({
       cep: String(data.cep).replace("-", ""),
       logradouro: data.logradouro,
       complemento: data.complemento,
       bairro: data.bairro,
       localidade: data.localidade,
       uf: data.uf,
-    };
-
-    const cepInfo = await CepInfoModel.create(cep);
+    });
 
     return res.status(200).json({
       success: true,
-      data: cepInfo,
+      cepInfo,
     });
   } catch (err) {
     return res.status(500).json({
